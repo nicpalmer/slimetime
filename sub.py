6e27f021894c4d5385779872042844b4eb5267f6
nicpalmer <Nicholasjamespalmer@googlemail.com>
2016-02-18 12:54:01 +0000

    Create sub.py
    A simple python/zmq subscriber, note start this before the publisher or else you'll lose precious logs. 
    
    # next version will introduce logic checks.

---
+++ sub.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import zmq
+
+port = "5556"
+if len(sys.argv) > 1:
+    port =  sys.argv[1]
+    int(port)
+
+if len(sys.argv) > 2:
+    port1 =  sys.argv[2]
+    int(port1)
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+socket.connect ("tcp://localhost:%s" % port)
